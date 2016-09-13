4418bdcc376f71227a23cb4514d425ae46386363
Luke Lai <uklelai@gmail.com>
2016-09-12 17:38:31 -0700

    Create sounds.js

---
+++ sounds.js
@@ -0,0 +1,7 @@
+var sm = new SoundManager();
+var changelevel = sm.load("http://picosong.com/2zBV/");
+	//changelevel.play();
+var placement = sm.load("http://picosong.com/2zBJ/");
+	//placement.play();
+var select = sm.load("http://picosong.com/2zBh/");
+	//select.play();
